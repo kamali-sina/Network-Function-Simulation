3f99020051bc20a88373283642a36ec5b80e5e6d
sina-negarandeh <negarandeh.sina@gmail.com>
2021-04-29 22:31:09 +0430

    Add command file descriptor to system

--- System.cpp
+++ System.cpp
@@ -2,11 +2,16 @@
 
 using namespace std;
 
-System::System(int system_number){
+System::System(int system_number, int command_fd){
     system_number_ = system_number;
+    command_fd_ = command_fd;
 }
 
 int System::connect(int switch_number, int port_number){
     // TODO: do something here!
     return 1;
+}
+
+int System::getCommandFd() {
+    return command_fd_;
 }
--- System.hpp
+++ System.hpp
@@ -3,12 +3,14 @@
 
 class System{
     public:
-    System(int system_number);
+    System(int system_number, int command_fd);
     int connect(int switch_number, int port_number);
     int get_number(){return system_number_;}
+    int getCommandFd();
 
     private:
     unsigned int system_number_;
+    int command_fd_;
 };
 
 
