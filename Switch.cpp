5dcdc62fa7399c300cf246da7202d355835fb62f
sina-negarandeh <negarandeh.sina@gmail.com>
2021-04-29 22:59:55 +0430

    Add command file descriptor to switch

--- Switch.cpp
+++ Switch.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-Switch::Switch(int number_of_ports, int switch_number){
+Switch::Switch(int number_of_ports, int switch_number, int command_fd){
     number_of_ports_ = number_of_ports;
     switch_number_ = switch_number;
+    command_fd_ = command_fd;
 }
 
 int Switch::connect(int system_number, int port_number){
     // TODO: do something here!
     return 1;
+}
+
+int Switch::getCommandFd() {
+    return command_fd_;
 }
--- Switch.hpp
+++ Switch.hpp
@@ -10,14 +10,16 @@ struct lookup{
 
 class Switch{
     public:
-    Switch(int number_of_ports, int switch_number);
+    Switch(int number_of_ports, int switch_number, int command_fd);
     int connect(int system_number, int port_number);
     int get_number(){return switch_number_;}
+    int getCommandFd();
 
     private:
     unsigned int number_of_ports_;
     unsigned int switch_number_;
     std::vector<lookup> lookup_table_;
+    int command_fd_;
 };
 
 #endif
